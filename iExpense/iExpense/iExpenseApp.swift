2861f33ebfcf26827efc836d06b78c06075bbfd1
Anup Deshpande <deshpande.anup24@gmail.com>
2020-11-02 16:44:49 -0500

    docs: Add readme to Better Rest App

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anup Deshpande on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
